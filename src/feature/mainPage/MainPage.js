3acc9743dbdf4b5bcbf1da36b45c957537ca4e01
jiawa0 <1ee1ee997mm@gmail.com>
2023-08-19 21:44:33 +0800

    移除註解

--- src/feature/mainPage/MainPage.js
+++ src/feature/mainPage/MainPage.js
@@ -13,20 +13,6 @@ const MainPage = () => {
 
   return (
     <$.PageWrapper>
-      {/* <CustomInputNumber
-        min={1}
-        max={20}
-        step={2}
-        name={'inputName'}
-        value={5}
-        disabled={false}
-        onChange={(event) => {
-          console.log(event.target.value, '=====', event.target.name);
-        }}
-        onBlur={(event) => {
-          console.log(event.target.value);
-        }}
-      /> */}
       <RoomAllocation
         guest={10}
         room={3}
